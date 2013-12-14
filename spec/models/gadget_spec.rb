bca61b1315dbe31421f850250cb0038c15d4d215
Mohamed Magdy <eng.mohamagdy@gmail.com>
2013-12-14 16:18:17 +0100

    #11 search gadgets tests added

--- spec/models/gadget_spec.rb
+++ spec/models/gadget_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe Gadget do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe "search" do
+		before(:each) do
+			@gadget_name = "Gadget"
+			@gadget = FactoryGirl.create(:gadget, name: @gadget_name)
+		end
+
+		it "should return the matching gadgets by name" do
+			gadgets =  Gadget.search({ name: @gadget_name})
+
+			gadget.should_not be_empty
+			gadget.should include(@gadget)
+		end
+
+		it "should not return any matching gadgets by name" do
+			gadgets =  Gadget.search({ name: "Another Gadget"})
+
+			gadget.should be_empty
+			gadget.should_not include(@gadget)
+		end
+	end
 end
