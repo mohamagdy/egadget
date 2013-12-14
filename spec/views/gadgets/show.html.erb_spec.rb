8584e051fd04dc2c682a1a06164ed5c26e9c15bb
Mohamed Magdy <eng.mohamagdy@gmail.com>
2013-12-14 17:07:17 +0100

    #5 attachment displayed in show page for the gadget test

--- spec/views/gadgets/show.html.erb_spec.rb
+++ spec/views/gadgets/show.html.erb_spec.rb
@@ -5,8 +5,17 @@ describe "gadgets/show" do
     @gadget = FactoryGirl.create(:gadget)
   end
 
-  it "renders attributes in <p>" do
+  it "renders attributes in <p> without attachment" do
     render
     expect(rendered).to match(/Name/)
+    assert_select "img", count: 0
+  end
+
+  it "renders attributes in <p> without attachment" do
+    @gadget.attachment_ids = [FactoryGirl.create(:attachment).id]
+    
+    render
+    expect(rendered).to match(/Name/)
+    assert_select "img", count: 1
   end
 end
